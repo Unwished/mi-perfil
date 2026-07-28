bfe8202e047e10f5a2ff19d35da92c0691783043
Unwished <santiago.alvarez.2k7@gmail.com>
2026-07-27 21:50:46 -0500

    Presentación basica

--- App.js
+++ App.js
@@ -1,11 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={{ width: 360, height: 260, backgroundColor: '#fff', borderRadius: 40, marginBottom: 20 , alignItems: 'center'}}>
+        
+        <Image source={require('./assets/Foto.png')} style={{ width: 100, height: 100, borderRadius: 50, marginTop: 20 }} />
+        
+        <Text>Nombre: Santiago Álvarez Maffiold </Text>
+        <Text>Edad: 19 años</Text>
+        <Text>Rol: Estudiante</Text>
+        <Text>Habilidades: {"\n"} - Responsable {"\n"} - Liderazgo {"\n"} - Trabajo en equipo</Text>
+     
+        <StatusBar style="auto" />
+      </View>
     </View>
   );
 }
@@ -13,7 +22,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#060018',
     alignItems: 'center',
     justifyContent: 'center',
   },
